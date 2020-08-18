2604e1c985da559e6c230802e8db80961dabe024
Sierra Chapman <siechap@gmail.com>
2020-08-18 14:32:42 -0700

    add invite_code to household model

--- models/household.js
+++ models/household.js
@@ -1,16 +1,34 @@
 // Creating our Household model
+
+const inviteCodeChars = "BCDFGHJKLMNPQRSTVWXYZ0123456789";
+const inviteCodeLength = 8;
+
 module.exports = function(sequelize, DataTypes) {
     const Household = sequelize.define("Household", {
         name: {
             type: DataTypes.STRING,
-            allowNull: false
-        }
+            allowNull: false,
+        },
+        invite_code: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     });
     Household.associate = function (models) {
         Household.hasMany(models.User);
         Household.hasMany(models.Chore, {
           onDelete: "cascade"
         });
-      };
+    };
+    Household.addHook("beforeCreate", function(household) {
+        let invite_code = "";
+
+        for (let i = 0; i < inviteCodeLength; i++) {
+            const randomIndex = Math.floor(Math.random() * inviteCodeChars.length);
+            invite_code += inviteCodeChars[randomIndex];
+        }
+
+        household.invite_code = invite_code;
+    });
     return Household
 };
