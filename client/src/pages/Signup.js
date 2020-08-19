720ad593af0c47296b5a8b793474682da572ebaa
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-19 13:51:52 -0700

    fixing signin error again

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,10 +1,6 @@
 import React from "react";
-<<<<<<< HEAD
 import SignUpForm from "../components/SignUp";
-=======
-import SignUpForm from "../components/Signup";
 import { Container } from "../components/Grid";
->>>>>>> 56350727aa1c59a8d7ad52aa474d157ea6eaf60b
 
 
 const SignUp = () => {
