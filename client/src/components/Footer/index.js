31efbe2af87258df1abe07bb5b042355ba02fa7d
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-19 13:26:36 -0700

    working on footer

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Footer () {
+    return (
+<footer className="footer">
+      <div className="container">
+          <div className= "row">
+          <a className="navbar-brand" href="https://books.google.com/">Logo</a>
+          <h5 className="white-text" style="font-weight: 900;">Chore Hack</h5>
+          <p className="subtitle is-6"> <span className="icon">
+                    <i className="fas fa-broom"></i></span>Hannah</p>
+
+          </div>
+        
+      </div>
+    </footer>
+
+
+
+    );
+}
+
+export default Footer;
