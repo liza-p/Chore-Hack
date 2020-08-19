9f422432c5947646cf477bd9098d6df3031c9013
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-19 14:14:51 -0700

    got logo working in navbar and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,8 +5,9 @@ function Footer () {
 <footer className="footer">
       <div className="container">
           <div className= "row">
-          <a className="navbar-brand" src = "./img/aasdf"></a>
-          <h5 className="white-text" style={{}}>Chore Hack</h5>
+          <a class="navbar-brand" href="#">
+                <img src="../img/project3.png" style={{ width: "30px", height: "30px" }} className="d-inline-block align-top" alt="Chore Hack" /></a>
+          {/* <h5 className="white-text" style={{}}>Chore Hack</h5> */}
           <p className="subtitle is-6"> <span className="icon"><i className="fas fa-broom"></i></span>Ayla</p>
           <p className="subtitle is-6"> <span className="icon"><i class="fas fa-laptop-house"></i></span>Liza</p>
           <p className="subtitle is-6"> <span className="icon"><i class="fas fa-hand-sparkles"></i></span>Hannah</p>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,7 +3,10 @@ import React from "react";
 function Navbar (){
     return (
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#"><strong>Chore App</strong></a>
+          <a class="navbar-brand" href="#">
+    <img src="../img/project3.png" style = {{width: "30px", height:"30px"}} className="d-inline-block align-top" alt=""/>
+    Chore Hack
+  </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
@@ -19,7 +22,7 @@ function Navbar (){
                 <a className="nav-link" href=" ">Manage Chores</a>
             </li>
             <li className="nav-item active">
-                <a className="nav-link"  href=" ">Log Out</a>
+                <a className="nav-link" style={{float: "inline-end"}}  href=" ">Log Out</a>
             </li>
             </ul>
         </div>
