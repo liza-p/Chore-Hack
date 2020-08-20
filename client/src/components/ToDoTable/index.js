dbf9f9d5ca20c1fb3a2df56d76d4de0715a1e592
Ayla Dillis <aayladillis@gmail.com>
2020-08-20 13:47:29 -0700

    adding checkboxes

--- client/src/components/ToDoTable/index.js
+++ client/src/components/ToDoTable/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ToggleButtonGroupControlled from "../ControlledBtn"
-import { Tabs, Tab } from 'react-bootstrap';
+import ToggleButtonGroupControlled from "../ControlledBtn";
+import HouseholdBtn from "../HouseholdBtn";
+import { Form, Col, Row, Tabs, Tab } from 'react-bootstrap';
 import './style.css';
 
 
 function ToDoTable () {
   return (
+  <Form>
     <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
       <Tab eventKey="home" title="Your Chores">
         <p>
@@ -13,11 +15,13 @@ function ToDoTable () {
         </p>
       </Tab>
       <Tab eventKey="profile" title="Household's Chores">
-      <p>
-        <ToggleButtonGroupControlled />
+        <p>
+        {/* <ToggleButtonGroupControlled /> */}
+        <HouseholdBtn />
         </p>
       </Tab>
     </Tabs>
+  </Form>
   );
 }
 
