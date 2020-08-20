a937de01589f5d41deb79cbe96734868825f24da
Ayla Dillis <aayladillis@gmail.com>
2020-08-20 11:36:16 -0700

    adding checkboxes

--- client/src/components/ToDoTable/index.js
+++ client/src/components/ToDoTable/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import ReactDOM from "react-dom";
+import Checkbox from "../Checkbox"
+import './style.css';
 
 function ToDoTable () {
   return (
       <Tabs>
         <TabList>
-          <Tab>Your Chores</Tab>
+          <Tab class="tab">Your Chores</Tab>
           <Tab>Household Chores</Tab>
         </TabList>
-     
         <TabPanel>
-          <p>Any content 1</p>
+          <p>
+          <Checkbox />
+          </p>
         </TabPanel>
         <TabPanel>
           <p>Any content 2</p>
