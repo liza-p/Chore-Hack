28f8b5d5e6dd63adbac2238d4b54187e3d2848f5
Ayla Dillis <aayladillis@gmail.com>
2020-08-21 13:46:44 -0700

    creating global state

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,61 +1,86 @@
 import React, { createContext, useReducer, useContext } from "react";
 import {
-ADD_USER,
-UPDATE_USER,
-REMOVE_USER,
-ADD_HOUSEHOLD,
-UPDATE_HOUSEHOLD,
-REMOVE_HOUSEHOLD,
-ADD_CHORE,
-UPDATE_CHORE,
-REMOVE_CHORE
+    UPDATE_USERNAME,
+    UPDATE_HOUSEHOLD,
+    UPDATE_MEMBERS,
+    UPDATE_CHORES,
+    UPDATE_CHORE,
+    ADD_CHORE,
+    REMOVE_CHORE,
+    UPDATE_REPETITIONS,
+    COMPLETE_REPETITION,
+    UNDO_REPETITION
+
 } from './actions';
+import API from "./API";
+
+const ChoreContext = createContext();
+const { Provider } = ChoreContext;
 
 const reducer = (state, action) => {
     switch (action.type) {
-        case ADD_USER:
+        case UPDATE_USERNAME:            
             return {
-                ...state
-            };
+                ...state,
+                username: action.username
+            }
         
-        case UPDATE_USER:
-            return {
-                ...state
-            };
-
-        case REMOVE_USER:
-            return {
-                ...state
-            };
-        
-        case ADD_HOUSEHOLD:
-            return {
-                ...state
-            };
-
         case UPDATE_HOUSEHOLD:
             return {
-                ...state
+                ...state,
+                household: action.household
             };
 
-        case REMOVE_HOUSEHOLD:
+        case UPDATE_MEMBERS:
             return {
-                ...state
+                ...state,
+                members: action.members
+            };
+        
+        case UPDATE_CHORES:
+            return {
+                ...state,
+                chores: action.chores
+            };
+        
+        case UPDATE_CHORE:
+            return {
+                ...state,
+                chore: action.chore
             };
 
         case ADD_CHORE:
             return {
-                ...state
+                ...state,
+                chores: [...state.chores, action.chores],
+            };
+            
+        case REMOVE_CHORE:
+            return {
+                ...state,
+                id: (chores.id === action.id ? " " : chores.id)
             };
 
-        case UPDATE_CHORE:
+
+        case UPDATE_REPETITIONS:
             return {
                 ...state
             };
 
-        case REMOVE_CHORE:
+        case COMPLETE_REPETITION:
+            return {
+                ...state
+            };
+
+        case UNDO_REPETITION:
             return {
                 ...state
             }
     }
+}
+
+const ChoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = userReducer(reducer, {
+
+    })
 }
