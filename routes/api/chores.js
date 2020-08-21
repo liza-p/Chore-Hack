509e8e44215ac624407873a5b4c2ccbf6c5b08ac
Sierra Chapman <siechap@gmail.com>
2020-08-21 12:15:08 -0700

    edit chores controller

--- routes/api/chores.js
+++ routes/api/chores.js
@@ -4,9 +4,9 @@ const choresController = require("../../controllers/choresController");
 // Matches with "/api/chores"
 router
   .route("/")
-  .get(choresController.findAll) // get all chores
+  .get(choresController.findAll) // get all chores belonging to user's household
   .post(choresController.create) // create a chore
   .put(choresController.update) //update a chore
-  .delete(choresController.remove); // delete a chore
+  .delete(choresController.remove); // deactivate a chore
 
 module.exports = router;
