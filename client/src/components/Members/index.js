7b677227186e2cfa05bf4a3cb22682562d468e8b
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-21 14:14:49 -0700

    working on household

--- client/src/components/Members/index.js
+++ client/src/components/Members/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import "./style.css";
+import { Table } from "react-bootstrap";
 
 const Members = () => {
     return (
-        <div>
-
+        <div className="memberContainer">
+            <div className="row">
+                <div class="col-8">
+                    <Table striped bordered hover size="sm">
+                        <thead>
+                            <tr>
+                                <th>Color</th>
+                                <th>Name</th>
+                                <th>Notification</th>
+                                {/* <th>Assigned to:</th> */}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                {/* <td>@mdo</td> */}
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
+            </div>
         </div>
     );
 };
