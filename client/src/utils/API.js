b55386c1c1968bed1e7b8dc0254e425dd81e09bf
Sierra Chapman <siechap@gmail.com>
2020-08-20 20:30:31 -0700

    add functionality to login form

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,6 +4,9 @@ export default {
   signup: function(name, email, password) {
     return axios.post("/api/users/signup", { name, email, password });
   },
+  login: function(email, password) {
+    return axios.post("/api/users/login", { email, password });
+  },
   logout: function() {
     return axios.get("/api/users/logout");
   },
