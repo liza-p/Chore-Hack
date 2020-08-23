a64902ea4c833d2a2a4e69b4ce84cbbbc0992969
liza p <liyourlp@gmail.com>
2020-08-22 21:49:53 -0700

    add chore function

--- client/src/components/chores/index.js
+++ client/src/components/chores/index.js
@@ -4,16 +4,18 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import API from "../../utils/API";
 
-const RepeatedDays = () => {
+//function to render Repeated days checkboxes
+//it takes onToggleDay callback to call when any of the checkboxes are checked 
+const RepeatedDays = ({repeatedDays, onToggleDay}) => {
     return (
         <div className="row">
-            <div className="col-auto px-2">Mo<input type="checkbox"/></div>
-            <div className="col-auto px-2">Tu<input type="checkbox"/></div>
-            <div className="col-auto px-2">We<input type="checkbox"/></div>
-            <div className="col-auto px-2">Th<input type="checkbox"/></div>
-            <div className="col-auto px-2">Fr<input type="checkbox"/></div>
-            <div className="col-auto px-2">Sa<input type="checkbox"/></div>
-            <div className="col-auto px-2">Su<input type="checkbox"/></div>
+            <div className="col-auto px-2">Mo<input type="checkbox" checked={repeatedDays[0]} onChange={(e) => onToggleDay(0, e.target.checked)}/></div>
+            <div className="col-auto px-2">Tu<input type="checkbox" checked={repeatedDays[1]} onChange={(e) => onToggleDay(1, e.target.checked)}/></div>
+            <div className="col-auto px-2">We<input type="checkbox" checked={repeatedDays[2]} onChange={(e) => onToggleDay(2, e.target.checked)}/></div>
+            <div className="col-auto px-2">Th<input type="checkbox" checked={repeatedDays[3]} onChange={(e) => onToggleDay(3, e.target.checked)}/></div>
+            <div className="col-auto px-2">Fr<input type="checkbox" checked={repeatedDays[4]} onChange={(e) => onToggleDay(4, e.target.checked)}/></div>
+            <div className="col-auto px-2">Sa<input type="checkbox" checked={repeatedDays[5]} onChange={(e) => onToggleDay(5, e.target.checked)}/></div>
+            <div className="col-auto px-2">Su<input type="checkbox" checked={repeatedDays[6]} onChange={(e) => onToggleDay(6, e.target.checked)}/></div>
         </div>
         
     );
@@ -21,9 +23,27 @@ const RepeatedDays = () => {
 
 const Chores = () => {
     const [startDate, setStartDate] = useState(new Date());
+    const [choreName, setChoreName] = useState("");
+    const [assignedId, setAssignedId] = useState("");
     const [repeated, setRepeated] = useState(false);
     const [members, setMembers] = useState([]);
     const [repeatedDays, setRepeatedDays] = useState([false,false,false,false,false,false,false]);
+
+    const addChore = () => {
+      API.createChore(choreName, repeated, repeatedDays, assignedId)
+      .then(() => {
+        console.log("Chore is created!");
+        setChoreName("");
+        setRepeated(false);
+        setRepeatedDays([false,false,false,false,false,false,false]);
+      });
+    };
+    // change the repeated days state 
+    // function takes day param that represents days from M-Su and state of the checkbox
+    const toggleDay = (day, checked) => {
+      repeatedDays[day] = checked;
+      setRepeatedDays(repeatedDays);
+    };
 
     useEffect(() => {
         API.getMembers().then((res) => setMembers(res.data));
@@ -43,11 +63,20 @@ const Chores = () => {
   </thead>
   <tbody>
   <tr>
-      <td><input className="form-control" placeholder="Add a chore"/></td>
-      <td style={{width: "100px"}}><input type="checkbox" onChange={(e) => setRepeated(e.target.checked)} /></td>
-      <td style={{width: "360px"}}>{ repeated ? <RepeatedDays/> : <DatePicker selected={startDate} onChange={date => setStartDate(date)} />}</td>
-      <td><select className="form-control" >{members.map((member, i) => <option key={i}>{member}</option>)}</select></td>
-      <td><button className="btn btn-primary">Add</button></td>
+      <td><input className="form-control" placeholder="Add a chore" value={choreName} onChange={(e) =>setChoreName(e.target.value)}/></td>
+      <td style={{width: "100px"}}><input type="checkbox" checked={repeated} onChange={(e) => setRepeated(e.target.checked)} /></td>
+      <td style={{width: "360px"}}>{
+        repeated
+        ? <RepeatedDays repeatedDays={repeatedDays} onToggleDay={toggleDay}/>
+        : <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
+      }</td>
+      <td>
+        <select className="form-control" onChange = {(e) => setAssignedId(e.target.value)}>
+        <option value="">Select an assignee...</option>
+          {members.map((member, i) => <option value={member.id} key={i}>{member.name}</option>)}
+        </select>
+      </td>
+      <td><button className="btn btn-primary" onClick={addChore}>Add</button></td>
     </tr>
     <tr>
       <td>1</td>
