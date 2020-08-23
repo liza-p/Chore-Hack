44a54374cf23406c403e378325ef16eb4a715ba1
Hannah Smith <hannahpsmith1@gmail.com>
2020-08-23 13:39:06 -0700

    got new calendar working

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ToDoTable from "../components/ToDoTable";
-import Calendar from "../components/Calendar";
+// import Calendar from "../components/Calendar";
 import Charts from "../components/Charts";
 import { Container } from "../components/Grid";
+import Calendar from "../components/calendarr/demo"
 
 
 const Dashboards = () => {
