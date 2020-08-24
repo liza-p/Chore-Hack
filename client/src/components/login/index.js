1630b6b28973589aa4b0cb50b1d386d53881fea9
Ayla Dillis <aayladillis@gmail.com>
2020-08-24 10:12:59 -0700

    adding hero img

--- client/src/components/login/index.js
+++ client/src/components/login/index.js
@@ -4,6 +4,9 @@ import "./Login.css";
 import { Link } from "react-router-dom";
 import ErrorMessage from "../ErrorMessage";
 import API from "../../utils/API";
+import { Card } from 'react-bootstrap';
+// import photo from '../public/img/hero-photo.jpg';
+
 
 const LoginForm = ({ refreshUserData }) => {
     const emailRef = useRef();
@@ -28,36 +31,61 @@ const LoginForm = ({ refreshUserData }) => {
                 refreshUserData();
                 setRedirect("/dashboard");
             })
-            .catch(err => {
-                // show error message depending on error type
-                if (!err.response) {
-                    setError("Unable to connect to the server.");
-                } else if (err.response.status === 401) {
-                    setError("Invalid email or password.");
-                } else {
-                    setError("An unknown error occurred.");
-                }
-                passwordRef.current.value = "";
-                console.log(err);
-            })
+            // .catch(err => {
+            //     show error message depending on error type
+            //     if (!err.response) {
+            //         setError("Unable to connect to the server.");
+            //     } else if (err.response.status === 401) {
+            //         setError("Invalid email or password.");
+            //     } else {
+            //         setError("An unknown error occurred.");
+            //     }
+            //     passwordRef.current.value = "";
+            //     console.log(err);
+            // })
     }
 
     return (
         redirect ? <Redirect to={redirect} /> :
-        <form className="mt-3">
-            <ErrorMessage message={error} />
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} />
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" ref={passwordRef} />
-            </div>
-            <button className="btn btn-primary" onClick={event => handleSubmit(event)}>Login</button>
-            <br></br>
-            <Link to="/signup">New user? Sign up</Link>
-        </form>
+            <Card className="bg-dark text-white" style={{ margin: '0px' }}>
+                <Card.Img src={process.env.PUBLIC_URL + "/img/hero-img2.jpg"} alt="Card image" />
+                <Card.ImgOverlay>
+                    <Card.Text>
+                        <Card body style={{ opacity: 0.9, marginTop: '120px', marginLeft: '90px', marginRight: '90px' }}>
+                        <form className="m-3 mt-3" >
+                            <ErrorMessage message={error} />
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control" id="exampleInputPassword1" ref={passwordRef} />
+                            </div>
+                            <button className="btn btn-primary" onClick={event => handleSubmit(event)}>Login</button>
+                            <br></br>
+                            <Link to="/signup">New user? Sign up</Link>
+                        </form>
+                        </Card >
+                    </Card.Text>
+                </Card.ImgOverlay>
+            </Card>
+
+
+            // <form className="m-3 mt-3">
+            //     <ErrorMessage message={error} />
+            //     <div className="form-group">
+            //         <label htmlFor="exampleInputEmail1">Email address</label>
+            //         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" ref={emailRef} />
+            //     </div>
+            //     <div className="form-group">
+            //         <label htmlFor="exampleInputPassword1">Password</label>
+            //         <input type="password" className="form-control" id="exampleInputPassword1" ref={passwordRef} />
+            //     </div>
+            //     <button className="btn btn-primary" onClick={event => handleSubmit(event)}>Login</button>
+            //     <br></br>
+            //     <Link to="/signup">New user? Sign up</Link>
+            // </form>
     )
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -11,3 +11,4 @@ const Login = props => {
 };
 
 export default Login;
+
