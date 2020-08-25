62c6bf7c68a6d328ce129fc974666c5eb8a284ae
Ayla Dillis <aayladillis@gmail.com>
2020-08-25 12:14:29 -0700

    copy btn

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,7 +1,6 @@
-// import React from "react";
 import React, { useRef, useState } from 'react';
 // import { Link } from "react-router-dom";
-import { Jumbotron, InputGroup, FormControl, Button, Toast } from "react-bootstrap";
+import { Jumbotron, InputGroup, Toast, FormControl, Button } from "react-bootstrap";
 import "./style.css";
 import { useChoreContext } from "../../utils/GlobalState";
 import JoinBtn from "../Invite/modal";
@@ -11,16 +10,18 @@ function Code() {
     const state = useChoreContext()[0];
     const [copySuccess, setCopySuccess] = useState('');
     const textAreaRef = useRef(null);
-// copy function
+    const [show, setShow] = useState(false);
     function copyToClipboard(e) {
         textAreaRef.current.select();
         document.execCommand('copy');
         e.target.focus();
         setCopySuccess('Copied!');
-      };
-    
+        setShow(true);
+    };
+
     return (
         <Jumbotron>
+
             <h1> {state.household} <span className="icon"><i className="fas fa-house-user"></i></span></h1>
             <div>
                 <br />
@@ -30,20 +31,20 @@ function Code() {
                     <FormControl
                         readOnly
                         ref={textAreaRef}
-                        value= {state.inviteCode}
+                        value={state.inviteCode}
                         aria-label="Default"
                         aria-describedby="inputGroup-sizing-default"
                     />
 
-                {
-                document.queryCommandSupported('copy') &&
-                    <div>
-                    <Button onClick={copyToClipboard}>Copy</Button> 
-                    {copySuccess}
-                    </div>
-                }
-
-                {/* <InputGroup.Text id="inputGroup-sizing-default" className="ml-2" >Copy</InputGroup.Text> */}
+                    {
+                        document.queryCommandSupported('copy') &&
+                        <div>
+                            <Button onClick={copyToClipboard} style={{ marginLeft: 5 }} >Copy</Button>
+                            <Toast onClose={() => setShow(false)} show={show} delay={3000} autohide className="position-absolute" style={{ right: -22, marginTop: 10 }}>
+                                <Toast.Body>Code copied!</Toast.Body>
+                            </Toast>
+                        </div>
+                    }
                 </InputGroup>
                 <JoinBtn />
                 <h5>Join a different Household</h5>
