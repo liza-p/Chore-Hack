f1048ede0acaaa74395e8b7593966f954726b6c9
liza p <liyourlp@gmail.com>
2020-08-24 22:57:18 -0700

    add HouseholdReps and User reps to show chores

--- client/src/components/ToDoTable/index.js
+++ client/src/components/ToDoTable/index.js
@@ -1,22 +1,68 @@
-import React from "react";
-import ToggleButtonGroupControlled from "../ControlledBtn";
-import HouseholdBtn from "../HouseholdBtn";
+import React, {useEffect} from "react";
+import Repetitions from "../Repetitions";
 import { Tabs, Tab } from 'react-bootstrap';
 import './style.css';
-
+import { useChoreContext } from "../../utils/GlobalState";
+import { UPDATE_REPETITIONS } from "../../utils/actions";
+import API from "../../utils/API";
 
 function ToDoTable() {
+
+  const [state, dispatch] = useChoreContext();
+
+  const loadRepetitions =() =>{
+
+    API.getAllRepetitions()
+    .then(res => {
+      dispatch({ type: UPDATE_REPETITIONS, repetitions: res.data });
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({ type: UPDATE_REPETITIONS, repetitions: [] });
+    });
+  }
+
+  useEffect(() => {
+    loadRepetitions();
+  }, []);
+
   return (
     <div className="m-4 border rounded" >
         <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
           <Tab eventKey="home" title="Your Chores">
-            <ToggleButtonGroupControlled />
+            <UserReps />
           </Tab>
           <Tab eventKey="profile" title="Household's Chores">
-            <HouseholdBtn />
+            <HouseholdReps />
           </Tab>
         </Tabs>
     </div>
+  );
+}
+
+function HouseholdReps() {
+  const [state, dispatch] = useChoreContext();
+
+  const repsWithChores = state.repetitions.map((repetition) => {
+    const chore = state.chores.find((chore) => chore.id === repetition.ChoreId);
+    return {...repetition, chore: chore}
+  });
+  console.log(repsWithChores);
+  return (
+    <Repetitions reps={repsWithChores} />
+  );
+}
+
+function UserReps() {
+  const [state, dispatch] = useChoreContext();
+
+  const repsWithChores = state.repetitions.filter((repetition) => repetition.UserId === 4).map((repetition) => {
+    const chore = state.chores.find((chore) => chore.id === repetition.ChoreId);
+    return {...repetition, chore: chore}
+  });
+
+  return (
+    <Repetitions reps={repsWithChores} />
   );
 }
 
