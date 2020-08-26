4f80ac7df81cbda273ed794283a4a72a391f77af
liza p <liyourlp@gmail.com>
2020-08-25 21:16:25 -0700

    add name and due_date to the to do table

--- client/src/components/Repetitions/index.js
+++ client/src/components/Repetitions/index.js
@@ -1,21 +1,39 @@
 import React from "react";
 import { Form } from 'react-bootstrap';
+import { useChoreContext } from "../../utils/GlobalState";
+import './style.css';
 
 
-
+const formatDate = (timestamp) =>{
+  if(!timestamp){
+    return "";
+  }
+  return (new Date(timestamp)).toDateString();
+}
 function Repetitions (props) {
+  const [state, dispatch] = useChoreContext();
 
+  console.log("Props", props.reps)
   return (
     <Form className="m-3">
-    {props.reps.map((rep) => (
+    {props.reps.map((rep) => {
+      const user = state.members.find((user) => user.id === rep.Chore?.UserId);
+      return (
         <div key={rep.id} className="mb-3">
         <Form.Check 
             type= 'checkbox'
-            label={rep.Chore?.chore}
+            label={
+              <span>
+                <span class="badge badge-pill badge-primary mr-1">{user?.name}</span>
+                {rep.Chore?.chore} 
+                <span className='rep-date ml-2'>{formatDate(rep.due_date)}</span>
+              </span>
+            }
         />
         </div>
         
-    ))}
+      );
+    })}
     </Form>
 
   );
