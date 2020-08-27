5f65c129585e31b985ecf20da9cc31edec219243
Sierra Chapman <siechap@gmail.com>
2020-08-26 21:07:31 -0700

    remove nav links when logged out

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -22,28 +22,35 @@ function Navbar() {
                 <img src="../img/project3.png" style={{ width: "45px", height: "45px" }} className="d-inline-block" alt="" />
                 Chore Hack
             </Link>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/dashboard" >Dashboard</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/household">Manage Household</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" activeClassName="active" to="/chores">Manage Chores</NavLink>
-                    </li>
-                </ul>
-            </div>
-            {state.username ? 
-            <span>
-                <span className="mr-2">Logged in as {state.username}</span>
-                <Button variant="primary" onClick={handleLogout}>Log Out</Button> 
-                </span> :
-            <Link to="/login"><Button variant="primary">Sign in</Button></Link>
+            {
+            state.username ? 
+                <>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/dashboard" >Dashboard</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/household">Manage Household</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeClassName="active" to="/chores">Manage Chores</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                    <span className="mr-3">Logged in as {state.username}</span>
+                    <Button variant="primary" onClick={handleLogout}>Log Out</Button> 
+                </> :
+                <>
+                    <div className="collapse navbar-collapse" />
+                    <div>
+                        <Link to="/login" className="mr-3">Log in</Link>
+                        <Link to="/signup"><Button variant="primary">Sign up</Button></Link>
+                    </div>
+                </>
             }
         </nav>
     )
