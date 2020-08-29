be9c1562be7f18a1787e50b591a7f8140845123b
liza p <liyourlp@gmail.com>
2020-08-28 21:06:07 -0700

    barchar colors and percentage

--- client/src/components/BarChart/index.js
+++ client/src/components/BarChart/index.js
@@ -6,17 +6,27 @@ import { useChoreContext } from "../../utils/GlobalState";
 
 function BarChart () {
   const [state, dispatch] = useChoreContext();
-  const completedChores = {}
+  const completedChores = {};
+  const totalChores = {};
 
   for ( var i = 0; i < state.members.length; i++){
-    completedChores[state.members[i].id]=0
+    completedChores[state.members[i].id] = 0;
+    totalChores[state.members[i].id] = 0;
   };
+  const now = Date.now();
   for ( i = 0; i < state.repetitions.length; i++){
-    if(state.repetitions[i].complete){
-      completedChores[state.repetitions[i].UserId]++;
-    };
+    const repetitionDate = new Date(state.repetitions[i].due_date);
+    if (repetitionDate.getTime() < now && (now - repetitionDate.getTime()) < 7 * 24 * 3600 * 1000) {
+      if(state.repetitions[i].complete){
+        completedChores[state.repetitions[i].UserId]++;
+      }
+      totalChores[state.repetitions[i].UserId]++;
+    }
   };
   console.log(completedChores);
+
+
+console.log('state.repetitions', state.repetitions)
 
 const data = {
   labels: state.members.map(member => member.name),
@@ -24,13 +34,19 @@ const data = {
   datasets: [
     {
       label: 'Chores Completed',
-      backgroundColor: '#007bff',
-      backgroundColor: '#007bff',
-      borderColor: '#007bff',
-      borderWidth: 1,
-      hoverBackgroundColor: '#33B2FF ',
-      hoverBorderColor: '#007bff',
-      data: state.members.map(member => completedChores[member.id])
+      //borderColor: 'none',
+      //borderWidth: 1,
+      //hoverBackgroundColor: '#33B2FF ',
+      //hoverBorderColor: '#007bff',
+      data: state.members.map(member => {
+        const completed = completedChores[member.id];
+        const total = totalChores[member.id];
+        if (total === 0) {
+          return 0;
+        }
+        return completed / total;
+      }),
+      backgroundColor: state.members.map(member => member.color),
     }
   ]
 };
@@ -39,7 +55,27 @@ const data = {
    
       <div style={{ height: 500, width: 800, margin: 'auto'}}>
         <h4>Percentage of Chores Completed Over the Last Week</h4>
-        <HorizontalBar data={data} />
+        <HorizontalBar
+          data={data}
+          options={{
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [
+                {
+                  ticks: {
+                    min: 0,
+                    max: 1,
+                    callback: function (value) {
+                      return (value * 100).toFixed(0) + '%'; // convert it to percentage
+                    },
+                  },
+                },
+              ]
+            }
+          }}
+        />
       </div>
     )
     
