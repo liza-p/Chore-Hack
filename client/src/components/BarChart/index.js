b5a7b3d0874d559b9f55f9b18887f19c2a69b0f2
Sierra Chapman <siechap@gmail.com>
2020-08-31 12:38:25 -0700

    fix bug

--- client/src/components/BarChart/index.js
+++ client/src/components/BarChart/index.js
@@ -15,7 +15,6 @@ function BarChart () {
   };
   const now = new Date();
   let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1);
-  tomorrow = now;
   for ( i = 0; i < state.repetitions.length; i++){
     const repetitionDate = new Date(state.repetitions[i].due_date);
     if (repetitionDate.getTime() < tomorrow.getTime() && (tomorrow.getTime() - repetitionDate.getTime()) <= 7 * 24 * 3600 * 1000) {
